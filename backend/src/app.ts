b29a968724a80b30512355cc296affe461cc3e4f
Acho Arnold <a.ewin@geophy.com>
2020-08-21 23:10:45 +0200

    Added dot env

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express'
 import {Container} from './Container/Container'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 const app = express();
 const port = process.env.PORT || "8000";
